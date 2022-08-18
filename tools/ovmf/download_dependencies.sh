e31abb6aa70ac65884a98fbfdd0d4dcdb5ae4787
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-08-18 11:46:49 +0200

    [Tools] Use Aleph.im Grub fork to build OVMF (#6)
    Replaced the patches used to build Grub by the use of a fork.
    This will allow us to maintain patches more easily.

--- tools/ovmf/download_dependencies.sh
+++ tools/ovmf/download_dependencies.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DOWNLOAD_DIR="${SCRIPT_DIR}/downloads"
 PATCH_DIR="${SCRIPT_DIR}/patches"
 
-GRUB_GIT_REPOSITORY="https://git.savannah.gnu.org/git/grub.git"
-GRUB_COMMIT="cf3a3acff057f61e6562fe07f7370a01bc634240"
+GRUB_GIT_REPOSITORY="https://github.com/aleph-im/grub.git"
+GRUB_COMMIT="aleph/efi-secrets"
 GRUB_DIR="${DOWNLOAD_DIR}/grub"
 
 EDK2_GIT_REPOSITORY="https://github.com/tianocore/edk2.git"
@@ -18,10 +18,8 @@ EDK2_DIR="${DOWNLOAD_DIR}/edk2"
 git clone ${GRUB_GIT_REPOSITORY} "${GRUB_DIR}"
 
 # Apply patches
-GRUB_PATCH_DIR="${PATCH_DIR}/grub"
 pushd "${GRUB_DIR}" > /dev/null
 git checkout "${GRUB_COMMIT}"
-git am "${GRUB_PATCH_DIR}"/*.eml "${GRUB_PATCH_DIR}/0001-Add-linuxefi-module.patch" "${GRUB_PATCH_DIR}/0002-Use-Debian-version-of-linuxefi-patches.patch"
 popd > /dev/null
 
 # Download EDK2 (=OVMF)
