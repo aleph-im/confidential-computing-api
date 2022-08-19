4bfb1c09338bd12f581e3fe744b203cece6d1d52
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-08-19 15:55:42 +0200

    Feature: DB migration system (#8)
    Added support for alembic to manage DB migrations.
    Node owners can now restart the VM supervisor without losing
    the state of VMs.
    
    The first migration script creates the VM and firmware tables.

--- app/models/db.py
+++ app/models/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
+from toolkit.db_connection import get_db_url
 
-DATABASE_URL = "sqlite+aiosqlite:///./sevtool_api.db"
 
-engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+engine = create_async_engine(get_db_url(), future=True, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
 
