f29ba56c11f469c7763e2f3111b6891a58c5eb1f
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-08-19 18:23:42 +0200

    Feature: support multiple users
    Added a users table and a script to create new users.
    At runtime, the API now verifies the user password with
    passlib + bcrypt.
    
    Modified the authentication FastAPI dependency to make
    it return the full user object instead of just the username.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import Base
 from .platform import *
+from .users import *
 from .vm import *
---
+++ tools/users/create_user.py
@@ -0,0 +1,44 @@
+import argparse
+
+from passlib.context import CryptContext
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models.users import User
+
+
+def cli_parse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Create a new user and insert it in the DB."
+    )
+    parser.add_argument(
+        "--username",
+        "-u",
+        action="store",
+        type=str,
+        help="Username.",
+    )
+    parser.add_argument(
+        "--password",
+        "-p",
+        action="store",
+        type=str,
+        help="Password.",
+    )
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace):
+    engine = create_engine("sqlite:///./confidential_vms.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    password = pwd_context.hash(args.password)
+    user = User(username=args.username, hashed_password=password)
+    session.add(user)
+    session.commit()
+
+
+if __name__ == "__main__":
+    main(cli_parse())
